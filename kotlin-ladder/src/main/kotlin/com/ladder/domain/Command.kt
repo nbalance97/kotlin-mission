3f2c8ca99477e16fb52eb6d03422894f874a1290
nbalance97 <bhlee971@gmail.com>
2023-03-27 09:14:01 +0900

    feat: 리뷰 반영

--- kotlin-ladder/src/main/kotlin/com/ladder/domain/Command.kt
+++ kotlin-ladder/src/main/kotlin/com/ladder/domain/Command.kt
@@ -4,9 +4,7 @@ enum class Command(
     val value: String,
 ) {
     ALL("ALL"),
-    FINISH("FINISH"),
-    NONE("");
-
+    FINISH("FINISH");
     companion object {
         operator fun contains(command: String): Boolean = values().any { it.value == command }
     }
